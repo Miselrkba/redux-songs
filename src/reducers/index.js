3075563ffa3723fd07e98890a49f5af792a083d9
iii <miselrkba@gmail.com>
2020-11-04 13:20:59 +0100

    added actionCreators and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "All stars", duration: "3:05" },
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "La la", duration: "1:05" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
